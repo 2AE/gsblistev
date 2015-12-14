5418d9731e292df739378a8d41b93f28fdd2654c
root <root@debian>
2015-12-14 16:29:10 +0100

    listv

--- app/cache/dev/twig/ef/efc3e6e50f552cefbecf945a82d91539d658d2884bb0f1902eb809dcfc6f3d40.php
+++ app/cache/dev/twig/ef/efc3e6e50f552cefbecf945a82d91539d658d2884bb0f1902eb809dcfc6f3d40.php
@@ -22,32 +22,32 @@ class __TwigTemplate_49cb64cdce0b43f7249d28858cd0332d0b88f58c3e9a453091baa7384e1
 
     protected function doDisplay(array $context, array $blocks = array())
     {
-        $__internal_652cdeef5e45fdeb212bda0dd5b349a7427f96363e56e01d4dd323520d6a614b = $this->env->getExtension("native_profiler");
-        $__internal_652cdeef5e45fdeb212bda0dd5b349a7427f96363e56e01d4dd323520d6a614b->enter($__internal_652cdeef5e45fdeb212bda0dd5b349a7427f96363e56e01d4dd323520d6a614b_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "PgGsbFraisBundle:ListeVehicules:ListeVehicules.html.twig"));
+        $__internal_e331243381c542b9c898fcdd00c93210fb61a8d16fc518daa3dd9bf7757c8b5f = $this->env->getExtension("native_profiler");
+        $__internal_e331243381c542b9c898fcdd00c93210fb61a8d16fc518daa3dd9bf7757c8b5f->enter($__internal_e331243381c542b9c898fcdd00c93210fb61a8d16fc518daa3dd9bf7757c8b5f_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "PgGsbFraisBundle:ListeVehicules:ListeVehicules.html.twig"));
 
         $this->parent->display($context, array_merge($this->blocks, $blocks));
         
-        $__internal_652cdeef5e45fdeb212bda0dd5b349a7427f96363e56e01d4dd323520d6a614b->leave($__internal_652cdeef5e45fdeb212bda0dd5b349a7427f96363e56e01d4dd323520d6a614b_prof);
+        $__internal_e331243381c542b9c898fcdd00c93210fb61a8d16fc518daa3dd9bf7757c8b5f->leave($__internal_e331243381c542b9c898fcdd00c93210fb61a8d16fc518daa3dd9bf7757c8b5f_prof);
 
     }
 
     // line 2
     public function block_body($context, array $blocks = array())
     {
-        $__internal_ed2479b9fb61bafe9b51d93e6940baf72546144b2b142b054d6b78014e200d71 = $this->env->getExtension("native_profiler");
-        $__internal_ed2479b9fb61bafe9b51d93e6940baf72546144b2b142b054d6b78014e200d71->enter($__internal_ed2479b9fb61bafe9b51d93e6940baf72546144b2b142b054d6b78014e200d71_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+        $__internal_dca5ed8c6d0f6bc025b5af5697baf97314f5339191d23c45ed6f1c230bea2a89 = $this->env->getExtension("native_profiler");
+        $__internal_dca5ed8c6d0f6bc025b5af5697baf97314f5339191d23c45ed6f1c230bea2a89->enter($__internal_dca5ed8c6d0f6bc025b5af5697baf97314f5339191d23c45ed6f1c230bea2a89_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
 
         // line 3
         $this->displayBlock('bloc1', $context, $blocks);
         
-        $__internal_ed2479b9fb61bafe9b51d93e6940baf72546144b2b142b054d6b78014e200d71->leave($__internal_ed2479b9fb61bafe9b51d93e6940baf72546144b2b142b054d6b78014e200d71_prof);
+        $__internal_dca5ed8c6d0f6bc025b5af5697baf97314f5339191d23c45ed6f1c230bea2a89->leave($__internal_dca5ed8c6d0f6bc025b5af5697baf97314f5339191d23c45ed6f1c230bea2a89_prof);
 
     }
 
     public function block_bloc1($context, array $blocks = array())
     {
-        $__internal_690f59590bacff4ada13d06f5841b36fae339b5ab1adc6bbe2961795d1baa75e = $this->env->getExtension("native_profiler");
-        $__internal_690f59590bacff4ada13d06f5841b36fae339b5ab1adc6bbe2961795d1baa75e->enter($__internal_690f59590bacff4ada13d06f5841b36fae339b5ab1adc6bbe2961795d1baa75e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "bloc1"));
+        $__internal_01b01f7a495856979f335889e94b77b4a97a38b4a7ade07579b13771954c3f13 = $this->env->getExtension("native_profiler");
+        $__internal_01b01f7a495856979f335889e94b77b4a97a38b4a7ade07579b13771954c3f13->enter($__internal_01b01f7a495856979f335889e94b77b4a97a38b4a7ade07579b13771954c3f13_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "bloc1"));
 
         // line 4
         echo "<div id=\"contenu\">
@@ -59,24 +59,23 @@ class __TwigTemplate_49cb64cdce0b43f7249d28858cd0332d0b88f58c3e9a453091baa7384e1
         $context['_seq'] = twig_ensure_traversable((isset($context["ListeVehiculesVisiteurs"]) ? $context["ListeVehiculesVisiteurs"] : $this->getContext($context, "ListeVehiculesVisiteurs")));
         foreach ($context['_seq'] as $context["_key"] => $context["uneL"]) {
             // line 8
-            echo "\t        <th><h3>";
+            echo "\t        <th><h3>nom: ";
             echo twig_escape_filter($this->env, $this->getAttribute($context["uneL"], "nom", array()), "html", null, true);
-            echo "</h3>
-            
-             matricule:";
-            // line 10
+            echo "</h3>  
+           \t <br/>matricule: ";
+            // line 9
             echo twig_escape_filter($this->env, $this->getAttribute($context["uneL"], "idnumImmat", array()), "html", null, true);
             echo "
-             <br/>marque:";
-            // line 11
+             <br/>marque: ";
+            // line 10
             echo twig_escape_filter($this->env, $this->getAttribute($context["uneL"], "marque", array()), "html", null, true);
             echo "
-             <br/>modele:";
-            // line 12
+             <br/>modele: ";
+            // line 11
             echo twig_escape_filter($this->env, $this->getAttribute($context["uneL"], "modele", array()), "html", null, true);
             echo "
-             <br/>couleur:";
-            // line 13
+             <br/>couleur: ";
+            // line 12
             echo twig_escape_filter($this->env, $this->getAttribute($context["uneL"], "couleur", array()), "html", null, true);
             echo "</th>
 
@@ -86,14 +85,14 @@ class __TwigTemplate_49cb64cdce0b43f7249d28858cd0332d0b88f58c3e9a453091baa7384e1
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_iterated'], $context['_key'], $context['uneL'], $context['_parent'], $context['loop']);
         $context = array_intersect_key($context, $_parent) + $_parent;
-        // line 17
+        // line 16
         echo "</table>\t\t\t
 
 </div>
 
 ";
         
-        $__internal_690f59590bacff4ada13d06f5841b36fae339b5ab1adc6bbe2961795d1baa75e->leave($__internal_690f59590bacff4ada13d06f5841b36fae339b5ab1adc6bbe2961795d1baa75e_prof);
+        $__internal_01b01f7a495856979f335889e94b77b4a97a38b4a7ade07579b13771954c3f13->leave($__internal_01b01f7a495856979f335889e94b77b4a97a38b4a7ade07579b13771954c3f13_prof);
 
     }
 
@@ -109,7 +108,7 @@ class __TwigTemplate_49cb64cdce0b43f7249d28858cd0332d0b88f58c3e9a453091baa7384e1
 
     public function getDebugInfo()
     {
-        return array (  90 => 17,  80 => 13,  76 => 12,  72 => 11,  68 => 10,  62 => 8,  58 => 7,  53 => 4,  41 => 3,  35 => 2,  11 => 1,);
+        return array (  89 => 16,  79 => 12,  75 => 11,  71 => 10,  67 => 9,  62 => 8,  58 => 7,  53 => 4,  41 => 3,  35 => 2,  11 => 1,);
     }
 }
 /* {% extends "PgGsbFraisBundle::accueil.html.twig" %}*/
@@ -119,12 +118,11 @@ class __TwigTemplate_49cb64cdce0b43f7249d28858cd0332d0b88f58c3e9a453091baa7384e1
 /* <h2>Historique des Véhicules</h2>*/
 /* <table>*/
 /* 	         {% for uneL in ListeVehiculesVisiteurs%}*/
-/* 	        <th><h3>{{ uneL.nom}}</h3>*/
-/*             */
-/*              matricule:{{ uneL.idnumImmat}}*/
-/*              <br/>marque:{{ uneL.marque}}*/
-/*              <br/>modele:{{ uneL.modele}}*/
-/*              <br/>couleur:{{ uneL.couleur}}</th>*/
+/* 	        <th><h3>nom: {{ uneL.nom}}</h3>  */
+/*            	 <br/>matricule: {{ uneL.idnumImmat}}*/
+/*              <br/>marque: {{ uneL.marque}}*/
+/*              <br/>modele: {{ uneL.modele}}*/
+/*              <br/>couleur: {{ uneL.couleur}}</th>*/
 /* */
 /*              */
 /*              {%endfor%}*/
